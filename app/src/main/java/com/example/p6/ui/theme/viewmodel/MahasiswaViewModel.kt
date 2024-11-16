b6b137403e81010e389f1b7bbcb345e9bec7d2fe
hikmah76 <nur.hikmah190504@gmail.com>
2024-11-16 23:25:20 +0700

    ViewModel untuk menyimpan data mahasiswa.

--- app/src/main/java/com/example/p6/ui/theme/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/p6/ui/theme/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.p6.ui.theme.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.p6.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(Mahasiswa())
+    val uiState: StateFlow<Mahasiswa> = _uiState.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _uiState.update { data ->
+            data.copy(
+                nama = ls[0],
+                nim = ls[1],
+                gender = ls[2],
+                alamat = ls[3],
+                email = ls[4],
+                noHp = ls[5]
+            )
+        }
+    }
 }
