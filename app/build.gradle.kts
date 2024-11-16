aca63c3903d5c65090facd89e6df4ccbd2dd73ea
hikmah76 <nur.hikmah190504@gmail.com>
2024-11-16 23:43:09 +0700

    pengaturan halaman

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Tambahan View Model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    //Tambahan Navigasi
+    implementation("androidx.navigation:navigation-compose:2.8.3")
 }
--- app/src/main/java/com/example/p6/Navigasi.kt
+++ app/src/main/java/com/example/p6/Navigasi.kt
@@ -1,4 +1,20 @@
 package com.example.p6
 
-class Navigasi {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.p6.ui.theme.viewmodel.MahasiswaViewModel
+
+enum class  Halaman {
+    Form,
+    Data
+}
+@Composable
+fun Navigasi (
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostcontroller = rememberNavContoller()
+
+) {
+
 }
